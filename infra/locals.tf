243e9866060a250b48148413b6da6a14aea39844
Alex Harvey <alexharv074@gmail.com>
2026-03-13 14:52:53 +1100

    Correction in github_repo

--- infra/locals.tf
+++ infra/locals.tf
@@ -2,7 +2,7 @@ locals {
   project     = "wiki-rag-azure"
   location    = "australiaeast"
   db_username = "wikirdb"
-  github_repo = "alex-harvey-z3q/wiki-rag"
+  github_repo = "alex-harvey-z3q/wiki-rag-azure"
 
   key_vault_name    = "wiki-rag-kv-ah"
   key_vault_rg_name = "wiki-rag-rg"
